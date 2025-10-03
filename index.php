9b88321561e93ad3918a9bb1a7418b728690248d
Adam Abiyu <advmabiyu@gmail.com>
2025-10-03 09:41:04 +0700

    nambahin ul dan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolor temporibus accusantium rerum quia, repudiandae perspiciatis provident modi quo, nemo dolorum, neque veniam. Est nulla, quos repellat aperiam quidem officia!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
